763be519bb2b64ea20636f6f914d4286b1682613
SpudGunMan <kellykeeton@yahoo.com>
2023-09-15 20:36:40 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,20 +1,25 @@
 # SPDX-FileCopyrightText: 2021 ladyada & Kattni Rembor for Adafruit Industries
 # sdr-keys: 2023 K7MHI
 
+# Setup:
+MYCALL = "K7MHI"
+
+# Lets Gooooooo!
 from adafruit_macropad import MacroPad
 import time
 
-macropad = MacroPad()
-last_position = 0
+#define objects and variables
+macropad = MacroPad() # Create the macropad object
+last_position = 0 # Rotary Encoder Position
 
-print("Thetis SDR Controller")
+print("SDR Keys v0.1")
 
 while True:
-    key_event = macropad.keys.events.get()
+    key_event = macropad.keys.events.get() # Get the key event
 
     if key_event:
         if key_event.pressed:
-            # Frequency Step
+            # Top Row Keyboard Event Keys
             if key_event.key_number == 0:
                 macropad.keyboard.send(macropad.Keycode.G)
                 macropad.keyboard.release_all()
@@ -28,7 +33,7 @@ while True:
             if key_event.key_number == 5:
                 macropad.keyboard.press(macropad.Keycode.R)
                 macropad.keyboard.release_all()
-            # Filter Step
+
             if key_event.key_number == 6:
                 macropad.keyboard.send(macropad.Keycode.V)
                 macropad.keyboard.release_all()
@@ -36,7 +41,7 @@ while True:
                 macropad.keyboard.press(macropad.Keycode.B)
                 macropad.keyboard.release_all()
                 
-            # Three Bottom Keys Volume Functions
+            # Three Bottom Keys Using Media Functions
             if key_event.key_number == 9:
                 macropad.consumer_control.send(
                     macropad.ConsumerControlCode.VOLUME_DECREMENT
@@ -50,7 +55,7 @@ while True:
                     macropad.ConsumerControlCode.VOLUME_INCREMENT
                 )
             
-            # MIDI Keys
+            # MIDI Key Events
             if key_event.key_number == 1:
                 macropad.midi.send(macropad.NoteOn(33, 127))
                 time.sleep(1.0)
@@ -67,11 +72,16 @@ while True:
                 macropad.midi.send(macropad.NoteOff(69, 0))
                 time.sleep(0.5)
             
-            
+    
+    # Rotary Encoder Button
     macropad.encoder_switch_debounced.update()
-    # Rotary Encoder PTT Behavior
     if macropad.encoder_switch_debounced.pressed:
-        macropad.keyboard.send(macropad.Keycode.SPACE)
+        #macropad.keyboard.send(macropad.Keycode.SPACE)
+        macropad.pixels.fill((0, 255, 0))
+        time.sleep(0.5)
+        macropad.pixels.fill((0, 0, 0))
+        print("Mode Select")
+        print("Thitus Mode")
 
     current_position = macropad.encoder
     # Rotary Encoder Behavior
